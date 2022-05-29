44f2a3fa30929eabee40bdd80043043c80eb1397
Tony <ferrug@yandex.ru>
2022-05-29 23:18:21 +0300

    homework ready

---
+++ arrays/src/seekAverageGreaterThanZero.java
@@ -0,0 +1,26 @@
+/* Implement the method that takes int[][] array and row index and
+checks if the average of the elements in the row is greater than 0 */
+
+public class seekAverageGreaterThanZero {
+    public static void main(String[] args) {
+        int[][] arr = {{-7, 2, -8, 4, 6,3,9}, {-7, 0, 8, -9, -1, 5,7}};
+        double sum = 0.0;
+        double result = 0.0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                int prev = arr[i][j];
+                sum = sum + prev;
+                result = sum / (arr.length * arr[0].length);
+            }
+        }
+        if (result > 0) {
+            System.out.println("the average is above zero");
+        }
+        else {
+            System.out.println("the average is below zero");
+        }
+    }
+}
+/*сложить все числа массива
+поделить сумму на количество элементов массива
+ */
